646e209aa3e54d286cf4b3bb317126646b0e2cd8
1shaman1 <narxq@yandex.ru>
2021-11-30 19:50:18 +0500

    Решение задачи

---
+++ CPU-bound.py
@@ -0,0 +1,22 @@
+import concurrent.futures
+from hashlib import md5
+from random import choice
+
+def genreate_coin(quantity):
+    count = 0
+    while count != quantity:
+        s = "".join([choice("0123456789") for i in range(50)])
+        h = md5(s.encode('utf8')).hexdigest()
+
+        if h.endswith("00000"):
+            count += 1
+            print(s, h)
+
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor(max_workers=100) as executor:
+        genreate_coin(5)
+
+
+if __name__ == '__main__':
+    main()
---
+++ IO-bound.py
@@ -0,0 +1,25 @@
+import concurrent.futures
+from urllib.request import Request, urlopen
+from urllib.parse import unquote
+
+links = open('res.txt', encoding='utf8').read().split('\n')
+
+def link_check(url):
+    request = Request(
+        url,
+        headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
+    )
+    resp = urlopen(request, timeout=5)
+    code = resp.code
+    resp.close()
+    return code
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    future_to_url = {executor.submit(link_check, url): url for url in links}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            print(future.result())
+        except Exception as e:
+            print(url, e)
+
